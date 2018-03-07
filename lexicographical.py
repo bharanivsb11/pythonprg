cdc1f81746bf87a6a881da8be847badaa849cdc6
bharanivsb11 <35220778+bharanivsb11@users.noreply.github.com>
2018-03-07 16:06:27 +0530

    lexicographical.py

--- lexicographical.py
+++ lexicographical.py
@@ -1,9 +1,13 @@
-a=int(raw_input())
+a=['3','4','7','2','1']
 n=len(a)
+p=0
 for i in range(0,n):
   for j in range(i+1,n):
     if(a[i]>a[j]):
       temp=a[i]
-      a[i]=a[i+1]
-      a[i+1]=temp
-print a[i]      
+      a[i]=a[j]
+      a[j]=temp
+for i in range(0,n):      
+  p=p+1
+if(p>=1):  
+  print a     
